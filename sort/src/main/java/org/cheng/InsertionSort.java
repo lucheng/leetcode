9b71821922ab9e6145a7d103a2fde78222258fd2
lucheng <1213136330@qq.com>
2020-07-16 08:03:32 +0800

    插入排序

---
+++ sort/src/main/java/org/cheng/InsertionSort.java
@@ -0,0 +1,31 @@
+package org.cheng;
+
+/**
+ * 插入排序
+ * 
+ * @author lucheng
+ *
+ */
+public class InsertionSort {
+	public int[] sort(int[] nums) {
+		for (int j = 1; j < nums.length; j++) {
+			int key = nums[j];
+			int i = j - 1;
+			while (i > 0 && nums[i] > key) {
+				nums[i + 1] = nums[i];
+				i--;
+			}
+			nums[i + 1] = key;
+		}
+		return nums;
+	}
+
+	public static void main(String[] args) {
+		InsertionSort s = new InsertionSort();
+		int[] nums = { -3, 40, -3, 90 };
+		int[] ts = s.sort(nums);
+		for (int i : ts) {
+			System.out.print(i+",");
+		}
+	}
+}
