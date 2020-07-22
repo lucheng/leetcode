03794916c88bc961bd8774a6a4ea15ec74f23fcf
ltpc0073 <ltpc0073@ltpc0073-PC>
2020-07-22 13:00:27 +0800

    完善

--- sort/src/main/java/com/cheng/ShellSort.java
+++ sort/src/main/java/com/cheng/ShellSort.java
@@ -9,16 +9,22 @@ package com.cheng;
 public class ShellSort {
 	public static Comparable<?>[] sort(Comparable<?>[] nums) {
 		int j;
+		Comparable<?> temp = null;
 		int increment = nums.length;
 		do {
 			increment = increment / 3 + 1;
-			for (int i = increment + 1; i < nums.length; i++) {
+			System.out.println("increment=" + increment + " ");
+			show(nums);
+			for (int i = increment; i < nums.length; i++) {
 				if (less(nums[i], nums[i - increment])) {
-					nums[0] = nums[i];
-					for (j = i - increment; j > 0 && less(nums[0], nums[j]); j -= increment) {
-						nums[j+increment] = nums[j];
+					temp = nums[i];
+					for (j = i - increment; j >= 0 && less(temp, nums[j]); j -= increment) {
+						nums[j + increment] = nums[j];
 					}
-					nums[j+increment] = nums[0];
+					nums[j + increment] = temp;
+					
+					System.out.println("i=" + i + " ");
+					show(nums);
 				}
 			}
 		} while (increment > 1);
@@ -31,14 +37,14 @@ public class ShellSort {
 	}
 
 	// 交换元素位置
-	public static void exch(Comparable[] a, int i, int j) {
-		Comparable t = a[i];
+	public static void exch(Comparable<?>[] a, int i, int j) {
+		Comparable<?> t = a[i];
 		a[i] = a[j];
 		a[j] = t;
 	}
 
 	// 单行中打印数组
-	public static void show(Comparable a[]) {
+	public static void show(Comparable<?> a[]) {
 		for (int i = 0; i < a.length; i++) {
 			System.out.print(a[i] + " ");
 		}
@@ -46,7 +52,7 @@ public class ShellSort {
 	}
 
 	public static void main(String[] args) {
-		Integer[] nums = { -3, 40, -4, 90 };
+		Integer[] nums = { 8, 1, 5, 9, 3, 7, 4, 6, 2 };
 		show(sort(nums));
 	}
 }
