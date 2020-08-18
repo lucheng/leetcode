be65896f04061f1799f13f1ac6d8f207e6f85685
lucheng <1213136330@qq.com>
2020-08-18 23:01:09 +0800

    图像渲染

--- 0733Flood-Fill/src/main/java/org/cheng/Solution.java
+++ 0733Flood-Fill/src/main/java/org/cheng/Solution.java
@@ -6,9 +6,41 @@ package org.cheng;
 public class Solution {
 
 	public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-		int color = image[sr-1][sc-1];
-		
+		int color = image[sr][sc];
+		if(color==newColor){
+			return image;
+		}
+
+		image[sr][sc] = newColor;
+
+		floodFill(image,sr,sc,color,newColor);
 		return image;
+	}
+
+	private void floodFill(int[][] image, int sr, int sc, int color,int newColor){
+		if(color==newColor){
+			return;
+		}
+
+		if(sr-1>=0 && sc>=0 && image[sr-1][sc]==color){
+			image[sr-1][sc] = newColor;
+			floodFill(image,sr-1,sc,color,newColor);
+		}
+
+		if(sr+1< image.length && sc>=0 && image[sr+1][sc]==color){
+			image[sr+1][sc] = newColor;
+			floodFill(image,sr+1,sc,color,newColor);
+		}
+
+		if(sc-1>=0 && sr>=0 && image[sr][sc-1]==color){
+			image[sr][sc-1] = newColor;
+			floodFill(image,sr,sc-1,color,newColor);
+		}
+
+		if(sc+1< image[0].length && sr>=0 && image[sr][sc+1]==color){
+			image[sr][sc+1] = newColor;
+			floodFill(image,sr,sc+1,color,newColor);
+		}
 	}
 
 	public static void main(String[] args) {
@@ -20,8 +52,9 @@ public class Solution {
 		int[][] newImage = s.floodFill(image,sr,sc,newColor);
 		for(int i=0;i<newImage.length;i++){
 			for(int j=0;j<newImage[i].length;j++){
-
+				System.out.print(newImage[i][j]);
 			}
+			System.out.println();
 		}
 		System.out.println(newImage);
 	}
