27ceb81f2ef278b09a27145d0ada9acfcfb115ac
lucheng <1213136330@qq.com>
2020-08-22 20:53:56 +0800

    Z

---
+++ 0006ZigZagConversion/src/main/java/org/cheng/Solution.java
@@ -0,0 +1,37 @@
+package org.cheng;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * 图像渲染
+ */
+public class Solution {
+
+	public String convert(String s, int numRows) {
+		if (numRows == 1) return s;
+
+		List<StringBuilder> rows = new ArrayList<>();
+		for (int i = 0; i < Math.min(numRows, s.length()); i++)
+			rows.add(new StringBuilder());
+
+		int curRow = 0;
+		boolean goingDown = false;
+
+		for (char c : s.toCharArray()) {
+			rows.get(curRow).append(c);
+			if (curRow == 0 || curRow == numRows - 1) goingDown = !goingDown;
+			curRow += goingDown ? 1 : -1;
+		}
+
+		StringBuilder ret = new StringBuilder();
+		for (StringBuilder row : rows) ret.append(row);
+		return ret.toString();
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		String str = "LEETCODEISHIRING";
+		System.out.println(s.convert(str,3));
+	}
+}
